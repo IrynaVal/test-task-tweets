465e9e8eb07fe324e14756411753391dab4c5ab7
IrynaVal <110104641+IrynaVal@users.noreply.github.com>
2023-04-25 21:45:40 +0000

    Deploying to gh-pages from @ IrynaVal/test-task-tweets@545443e3a07974ffee9f5926a95d91f3115c87ae 🚀

---
+++ static/js/983.4c444cc3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{983:function(e,t,r){r.r(t);var n=r(184);t.default=function(){return(0,n.jsx)(n.Fragment,{children:(0,n.jsx)("h2",{align:"center",children:"Welcome!"})})}}}]);
+//# sourceMappingURL=983.4c444cc3.chunk.js.map
--- static/js/983.5b80bc1d.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{983:function(e,t,r){r.r(t);var c=r(184);t.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)("h2",{children:"Welcome!"})})}}}]);
-//# sourceMappingURL=983.5b80bc1d.chunk.js.map
